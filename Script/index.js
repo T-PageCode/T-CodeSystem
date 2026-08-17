3759147660c4a994e44a4fdb522443f4559efff6
T-PageCode Developer <cdt20260@outlook.com>
2026-08-17 19:39:01 +0800

    增加点击导航栏的“体验”按钮触发的info_no_sys函数，增加“立即体验”按钮（系统依旧暂未开发）

--- Script/index.js
+++ Script/index.js
@@ -3,10 +3,17 @@ document.addEventListener("contextmenu",(e) => {
 })
 const aboutId = document.getElementById("about");
 const aboutOk = document.getElementById("about-ok");
+const infoNoSys = document.getElementById("info-no-sys");
 function about() {
     aboutId.style.visibility = "visible";
     aboutId.style.opacity = "1";
     aboutId.style.transform = "translate(-50%,-50%) scale(1)";
+}
+function info_no_sys() {
+    infoNoSys.style.right = "-190px";
+    setTimeout(() => {
+        infoNoSys.style.right = "-570px";
+    },7000)
 }
 aboutOk.onclick = () => {
     aboutId.style.opacity = "0";
