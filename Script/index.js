789c411f97d32af9d03aa1fb16f1cb72c14df50b
T-PageCode Developer <cdt20260@outlook.com>
2026-08-17 12:30:24 +0800

    加上关于界面的“确定”按钮过渡动画，点击“确定”后影藏“关于界面”的逻辑（上一个提交使用的transform动画从rotate改成了scale）

--- Script/index.js
+++ Script/index.js
@@ -2,8 +2,16 @@ document.addEventListener("contextmenu",(e) => {
     e.preventDefault();
 })
 const aboutId = document.getElementById("about");
+const aboutOk = document.getElementById("about-ok");
 function about() {
     aboutId.style.visibility = "visible";
     aboutId.style.opacity = "1";
     aboutId.style.transform = "translate(-50%,-50%) scale(1)";
+}
+aboutOk.onclick = () => {
+    aboutId.style.opacity = "0";
+    aboutId.style.transform = "translate(-50%,-50%) scale(0.8)";
+    setTimeout(() => {
+        aboutId.style.visibility = "hidden";
+    },500)
 }
