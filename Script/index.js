0b6282605077e0b08f5d5e8570f84c319900e363
T-PageCode Developer <cdt20260@outlook.com>
2026-08-17 12:18:50 +0800

    关于界面增加说明内容和确定按钮

--- Script/index.js
+++ Script/index.js
@@ -5,5 +5,5 @@ const aboutId = document.getElementById("about");
 function about() {
     aboutId.style.visibility = "visible";
     aboutId.style.opacity = "1";
-    aboutId.style.transform = "translate(-50%,-50%) rotate(0deg)";
+    aboutId.style.transform = "translate(-50%,-50%) scale(1)";
 }
