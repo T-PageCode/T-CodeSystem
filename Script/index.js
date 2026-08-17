90fea241e55ce317314704864539aa013e822b25
T-PageCode Developer <cdt20260@outlook.com>
2026-08-17 10:58:25 +0800

    关于界面的雏形，很多动画、文字、圆角没有写

--- Script/index.js
+++ Script/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("contextmenu",(e) => {
     e.preventDefault();
 })
+const aboutId = document.getElementById("about");
+function about() {
+    aboutId.style.visibility = "visible";
+    aboutId.style.opacity = "1";
+}
