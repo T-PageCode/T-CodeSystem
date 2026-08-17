dbfd288828424261529d851879f3cdc4b9ac7530
T-PageCode Developer <cdt20260@outlook.com>
2026-08-17 11:06:56 +0800

    补上关于界面的动画

--- Script/index.js
+++ Script/index.js
@@ -5,4 +5,5 @@ const aboutId = document.getElementById("about");
 function about() {
     aboutId.style.visibility = "visible";
     aboutId.style.opacity = "1";
+    aboutId.style.transform = "translate(-50%,-50%) rotate(0deg)";
 }
