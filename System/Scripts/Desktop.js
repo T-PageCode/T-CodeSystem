d9bb223fd734bfe7680d139b9faaa5bee26e1946
T-PageCode Developer <cdt20260@outlook.com>
2026-08-24 10:36:01 +0800

    右键菜单的“在GitHub查看”已写完，修复JS错误

--- System/Scripts/Desktop.js
+++ System/Scripts/Desktop.js
@@ -1,15 +1,18 @@
 let hideTimer = null;
 const mouse = document.getElementById("mouse");
 const menu = document.getElementById("menu");
-const menuItem = document.querySelectorAll(".menu-item").forEach((menuItemForEach) => {
-        menuItemForEach.onclick = () => {
-        menu.style.transform = "translate(-50%,-50%) scale(0.8)";
-        menu.style.opacity = "0";
-        hideTimer = setTimeout(() => {
-            menu.style.visibility = "hidden";
-        },500)
-    }
-})
+const githubLink = document.getElementById("github-link");
+function hiddenMenu() {
+    menu.style.opacity = "0";
+    menu.style.transform = "translate(-50%,-50%) scale(0.8)";
+    setTimeout(() => {
+        menu.style.visibility = "hidden";
+    },500)
+}
+githubLink.onclick = () => {
+    window.open("https://github.com/T-PageCode/T-CodeSystem", "_blank");
+    hiddenMenu();
+}
 document.addEventListener("contextmenu",(e) => {
     clearTimeout(hideTimer);
     e.preventDefault();
